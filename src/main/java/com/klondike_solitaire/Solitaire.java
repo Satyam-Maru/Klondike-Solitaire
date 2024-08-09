7a1357a66ace5b2b30ec97e43371eeea7272dd64
Satyam-Maru <satyammaru07@gmail.com>
2024-08-09 17:02:06 +0530

    Initial commit by satyam. 3

--- src/main/java/com/klondike_solitaire/Solitaire.java
+++ src/main/java/com/klondike_solitaire/Solitaire.java
@@ -9,6 +9,7 @@ public class Solitaire extends JFrame {
     protected static final int PANEL_WIDTH = 640, PANEL_HEIGHT = 500;
 
     public Solitaire(){
+        setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         gamePanel = new GamePanel();
         gamePanel.setPreferredSize(new Dimension(PANEL_WIDTH, PANEL_HEIGHT));
