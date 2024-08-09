dc53f3ebabfa848812a045a82ea9a0ce0703994e
Satyam-Maru <satyammaru07@gmail.com>
2024-08-09 17:22:37 +0530

    Pile class done.

--- src/main/java/com/klondike_solitaire/Pile.java
+++ src/main/java/com/klondike_solitaire/Pile.java
@@ -9,5 +9,35 @@ public abstract class Pile extends JPanel{
     protected int x, y;
     protected Stack<Card> cards;
 
+    public Pile(int x, int y){
+        super.setLocation(x, y);
+        cards = new Stack<>();
+    }
+
+    public Card topCard(){
+
+        if(!this.cards.isEmpty()){
+            return this.cards.peek();
+        }
+
+        return null;
+    }
+
+    public Card pop(){
+
+        try{
+            return cards.pop();
+        }catch (EmptyStackException e){
+            return null;
+        }
+    }
+
+    public void push(Card card){
+        this.cards.push(card);
+    }
+
+    public boolean isEmpty(){
+        return this.cards.isEmpty();
+    }
 
 }
