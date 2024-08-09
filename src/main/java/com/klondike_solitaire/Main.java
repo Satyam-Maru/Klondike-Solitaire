fbf41210c69770c52da09563576a73b6af3d1d81
Satyam-Maru <satyammaru07@gmail.com>
2024-08-09 16:54:48 +0530

    commit vs

--- src/main/java/com/klondike_solitaire/Main.java
+++ src/main/java/com/klondike_solitaire/Main.java
@@ -1,13 +1,10 @@
 package com.klondike_solitaire;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Satyam");
-        System.out.println("Inder");
-<<<<<<< HEAD
-        // inder--foundation,gamemoverlistner,gamepanel
-=======
-        // Manthan - Cards , Colour, Deck
->>>>>>> e1b7c363268597fb64b42b70b10ef6ce0c69cada
-    }
+        public static void main(String[] args) {
+                System.out.println("Satyam");
+                System.out.println("Inder");
+                // inder--foundation,gamemoverlistner,gamepanel
+                // Manthan - Cards , Colour, Deck
+        }
 }
