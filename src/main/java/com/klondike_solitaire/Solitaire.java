c1e029a8d88f808bcf84cbafe9e7153fdf8beba2
man1655 <manpatel1655@gmail.com>
2024-08-14 21:44:26 +0530

    Siza and length height change

--- src/main/java/com/klondike_solitaire/Solitaire.java
+++ src/main/java/com/klondike_solitaire/Solitaire.java
@@ -6,12 +6,14 @@ import java.awt.*;
 public class Solitaire extends JFrame {
 
     protected static GamePanel gamePanel;
-    protected static final int PANEL_WIDTH = 640, PANEL_HEIGHT = 500;
+    protected static final int PANEL_WIDTH = 600, PANEL_HEIGHT = 560;
 
     public Solitaire(){
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         gamePanel = new GamePanel();
         gamePanel.setPreferredSize(new Dimension(PANEL_WIDTH, PANEL_HEIGHT));
+        //setUndecorated(true);
+        setLocation(300,110);
         add(gamePanel);
         pack();
         setVisible(true);
