ba0083d187dd829237121b72b0a51f1a02d6174d
Satyam-Maru <satyammaru07@gmail.com>
2024-08-14 22:05:45 +0530

    Minor changes in width and height.

--- src/main/java/com/klondike_solitaire/Utility.java
+++ src/main/java/com/klondike_solitaire/Utility.java
@@ -1,4 +1,10 @@
 package com.klondike_solitaire;
 
 public class Utility {
+
+    // width and height of GamePanel is 600 x 600
+
+    // Top panel will occupy 600 width and 50 height
+
+    // Bottom panel will occupy 600 width and
 }
