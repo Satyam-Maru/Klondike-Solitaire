e3b28450cb1673836d41c7246cca7990171ba746
Inderjeet <148884138+Inderjeet1910@users.noreply.github.com>
2024-08-15 15:27:04 +0530

    color change

--- src/main/java/com/klondike_solitaire/Login.java
+++ src/main/java/com/klondike_solitaire/Login.java
@@ -61,7 +61,7 @@ public class Login extends JFrame implements ActionListener {
         this.setLayout(null);   // managing the layout self
         this.setLocationRelativeTo(null);   // sets the frame in center of the window
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.getContentPane().setBackground(Color.GRAY);
+        this.getContentPane().setBackground(Color.CYAN);
         this.add(mainPanel);
 
         // TO-DO apply threading in Database.getConnection()
@@ -79,7 +79,7 @@ public class Login extends JFrame implements ActionListener {
         mainPanel = new JPanel();
         mainPanel.setBounds(170, 80, frameWidth - 170, 420);
         mainPanel.setLayout(null);
-        mainPanel.setBackground(Color.GRAY);
+        mainPanel.setBackground(Color.CYAN);
 
         // setting the stock register image
         stockImgLabel = new JLabel();
