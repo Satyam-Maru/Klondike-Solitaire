bfe6b921cc329ebc34d039556eba6f9945a65c23
man1655 <manpatel1655@gmail.com>
2024-08-16 17:03:34 +0530

    Frame added to mid

--- src/main/java/com/klondike_solitaire/LeaderBoard.java
+++ src/main/java/com/klondike_solitaire/LeaderBoard.java
@@ -1,9 +1,19 @@
 package com.klondike_solitaire;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class LeaderBoard extends JFrame {
+    JPanel panel;
     LeaderBoard(){
+         panel=new JPanel();
+        panel.setBounds(0,0,440,440);
+        panel.setBackground(Color.white);
+        panel.setLayout(null);
+        add(panel);
+
+
+
         setUndecorated(true);
         setSize(450,450);
         setLocation(510,220);
