e1056f23d50cb1baf35a1359497c8ab8b437174f
man1655 <manpatel1655@gmail.com>
2024-08-16 16:37:05 +0530

    LaderBoard frame added

---
+++ src/main/java/com/klondike_solitaire/LeaderBoard.java
@@ -0,0 +1,15 @@
+package com.klondike_solitaire;
+
+import javax.swing.*;
+
+public class LeaderBoard extends JFrame {
+    LeaderBoard(){
+        setUndecorated(true);
+        setSize(450,450);
+        setLocation(340,190);
+        setVisible(true);
+        setLayout(null);
+
+    }
+
+}
